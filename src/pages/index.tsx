2db55ca77c26ea0400393226de099e80a12ac959
Eliezer Marques Mafra <eliezermmafra@live.com>
2023-01-27 15:16:07 -0300

    feat: Home with carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,55 @@
-import { styled } from "@/styles"
+import { HomeContainer, Product } from "@/styles/pages/home"
+import Image from "next/image"
+import { useKeenSlider } from 'keen-slider/react';
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
+import camiseta1 from '../assets/camisetas/1.png'
+import camiseta2 from '../assets/camisetas/2.png'
+import camiseta3 from '../assets/camisetas/3.png'
 
-  span: {
-    fontWeight: 'bold',
-  },
-
-  '&:hover': { 
-    filter: 'brightness(0.8)',
-   }
-
-})
+import 'keen-slider/keen-slider.min.css'
 
 export default function Home() {
-  return (
-    <Button>
-      <span>Teste</span> 
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 2.5,
+      spacing: 48,
+    }
+  })
 
-      Home</Button>
+  return (
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={camiseta1} width={520} height={480} alt={""}/>
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta2} width={520} height={480} alt={""}/>
+
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>R$ 59,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt={""}/>
+
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>R$ 100,00</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt={""}/>
+
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>R$ 100,00</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
