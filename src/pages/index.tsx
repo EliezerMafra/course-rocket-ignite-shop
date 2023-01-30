dff6d65bef559aebc04ae998ffbff35d6b3fd115
Eliezer Marques Mafra <eliezermmafra@live.com>
2023-01-30 11:06:48 -0300

    feat: add static props style: remove unused imports and files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,9 @@
 import { HomeContainer, Product } from "@/styles/pages/home"
 import { useKeenSlider } from 'keen-slider/react';
 import { stripe } from "@/lib/stripe";
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 
 import Image from "next/image"
-
-import camiseta1 from '../assets/camisetas/1.png'
-import camiseta2 from '../assets/camisetas/2.png'
-import camiseta3 from '../assets/camisetas/3.png'
 
 import 'keen-slider/keen-slider.min.css'
 import Stripe from "stripe";
@@ -47,7 +43,7 @@ export default function Home({ products }:  HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
@@ -67,6 +63,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       products
-    }
+    },
+    revalidate: 7200, // 2 hours
   }
 }
