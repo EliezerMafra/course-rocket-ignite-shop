b1fa97d994485da34e268d50fce55cdffb280392
Eliezer Marques Mafra <eliezermmafra@live.com>
2023-02-15 11:19:21 -0300

    feat: add styles to product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,24 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product"
+import Image from "next/image"
 import { useRouter } from "next/router"
 
-export default function Product(){
-  const {query} = useRouter()
+export default function Product() {
+  const { query } = useRouter()
 
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat consequatur est fugiat ut pariatur natus, voluptates earum ab rem in nisi corrupti magni molestiae minus similique repellat expedita quis impedit.</p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
